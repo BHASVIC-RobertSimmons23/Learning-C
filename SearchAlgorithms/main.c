4b6f91c7fa4be8608361813d30fdf6cf49809838
BHASVIC-RobertSimmons23 <145435727+BHASVIC-RobertSimmons23@users.noreply.github.com>
2024-09-10 09:59:26 +0100

    Binary Search

--- SearchAlgorithms/main.c
+++ SearchAlgorithms/main.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 
 int linearSearch(int target, const int* numArray, int length);
+int binarySearch(int target, const int* numArray, int length);
 
 int main() {
     int numArray[50];
@@ -11,7 +12,8 @@ int main() {
     bool found = false;
     srand(time(NULL));
     for(int i = 0; i < last + 1; i++) {
-        numArray[i] = rand() & 500;
+        //numArray[i] = rand() & 500;
+        numArray[i] = i;
         printf("%d) %d\n", i, numArray[i]);
     }
 
@@ -19,9 +21,9 @@ int main() {
     printf("Target: ");
     scanf("%d", &target);
 
-    int result = linearSearch(target, numArray, sizeof(numArray) / sizeof(numArray[0]));
+    int result = binarySearch(target, numArray, sizeof(numArray) / sizeof(numArray[0]));
     if(result == -1) printf("Element not in list");
-    else printf("%d found at index %d", target, result);
+    else printf("\n%d found at index %d", target, result);
     return 0;
 }
 
@@ -31,6 +33,17 @@ int linearSearch(int target, const int* numArray, int length) {
     while (foundIndex == -1 && i <= length) {
         if(numArray[i] == target) foundIndex = i;
         i++;
+    }
+    return foundIndex;
+}
+
+int binarySearch(int target, const int* numArray, int length) {
+    int low = 0, mid = 0, high = length - 1, foundIndex = -1, nComps = 0;
+    while (foundIndex == -1 && high >= low) {
+        mid = (low + high) / 2;
+        if(numArray[mid] < target) low = mid + 1;
+        else if(numArray[mid] > target) high = mid - 1;
+        else foundIndex = mid;
     }
     return foundIndex;
 }
