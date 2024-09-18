93cf92b77a574b81b44e4f66e3272296686fcbf1
BHASVIC-RobertSimmons23 <145435727+BHASVIC-RobertSimmons23@users.noreply.github.com>
2024-09-18 22:28:29 +0100

    Implemented merge sort

--- SearchAlgorithms/main.c
+++ SearchAlgorithms/main.c
@@ -5,45 +5,61 @@
 
 int linearSearch(int target, const int* numArray, int length);
 int binarySearch(int target, const int* numArray, int length);
+float testSearch(const int* numArray, int length, int (search)(const int, const int*, const int));
 
 int main() {
-    int numArray[50];
-    int first = 0, last = 49, value = 0;
-    bool found = false;
+    int arrayLength = 10000;
+    int numArray[arrayLength];
     srand(time(NULL));
-    for(int i = 0; i < last + 1; i++) {
-        //numArray[i] = rand() & 500;
-        numArray[i] = i;
-        printf("%d) %d\n", i, numArray[i]);
+    for(int i = 0; i < arrayLength; i++) {
+        if(i == 0) numArray[i] = rand() % 20 + 1;
+        else numArray[i] = numArray[i-1] + rand() % 20 + 1;
+        //printf("%d) %d\n", i, numArray[i]);
     }
 
-    int target = -1;
-    printf("Target: ");
-    scanf("%d", &target);
+    //int target = -1;
+    //printf("Target: ");
+    //scanf("%d", &target);
 
-    int result = binarySearch(target, numArray, sizeof(numArray) / sizeof(numArray[0]));
-    if(result == -1) printf("Element not in list");
-    else printf("\n%d found at index %d", target, result);
+    //int result = linearSearch(target, numArray, sizeof(numArray) / sizeof(numArray[0]));
+    //if(result == -1) printf("\nElement not in list");
+    //else printf("\n%d found at index %d", target, result);
+    //result = binarySearch(target, numArray, sizeof(numArray) / sizeof(numArray[0]));
+    //if(result == -1) printf("\nElement not in list");
+    //else printf("\n%d found at index %d", target, result);
+    float result = testSearch(numArray, sizeof(numArray) / sizeof(numArray[0]), linearSearch);
+    printf("\nLinear search took an average: %f comparisons", result);
+    result = testSearch(numArray, sizeof(numArray) / sizeof(numArray[0]), binarySearch);
+    printf("\nBinary search took an average: %f comparisons", result);
     return 0;
 }
 
-int linearSearch(int target, const int* numArray, int length) {
+int linearSearch(const int target, const int* numArray, int length) {
     int i = 0;
     int foundIndex = -1;
     while (foundIndex == -1 && i <= length) {
         if(numArray[i] == target) foundIndex = i;
         i++;
     }
-    return foundIndex;
+    return i;
 }
 
 int binarySearch(int target, const int* numArray, int length) {
-    int low = 0, mid = 0, high = length - 1, foundIndex = -1, nComps = 0;
+    int low = 0, mid, high = length - 1, foundIndex = -1, nComps = 0;
     while (foundIndex == -1 && high >= low) {
+        nComps++;
         mid = (low + high) / 2;
         if(numArray[mid] < target) low = mid + 1;
         else if(numArray[mid] > target) high = mid - 1;
         else foundIndex = mid;
     }
-    return foundIndex;
+    return nComps;
+}
+
+float testSearch(const int* numArray, int length, int (search)(const int, const int*, const int)) {
+    float total;
+    for(int i = 0; i < length; i++) {
+        total += search(numArray[i], numArray, length);
+    }
+    return total / length;
 }
---
+++ SortingAlgorithms/main.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+void printArray(int* array, int length);
+void bubbleSort(int* array, int length, bool verbose);
+void quickSort(int* array, int length);
+void insertionSort(int* array, int length, bool verbose);
+void mergeArrays(const int* left, int lenLeft, const int* right, int lenRight, int* output);
+void mergeSort(const int* array, int length, int* output);
+
+int main() {
+    int arrayLength = 100;
+    int numArray1[arrayLength];
+    int numArray2[arrayLength];
+    srand(time(NULL));
+    for(int i = 0; i < arrayLength; i++) {
+        int random = rand() % 500 + 1;
+        numArray1[i] = random;
+        numArray2[i] = random;
+    }
+    printArray(numArray1, arrayLength);
+    int output[arrayLength];
+    mergeSort(numArray1, arrayLength, output);
+    printf("\n");
+    printArray(output, arrayLength);
+    //insertionSort(numArray1, arrayLength, false);
+    //bubbleSort(numArray2, arrayLength, false);
+    printf("\n\n");
+    //printArray(numArray, arrayLength);
+    return 0;
+}
+
+void printArray(int* array, int length) {
+    for(int i = 0; i < length; i++) printf("%d, ", array[i]);
+}
+
+void bubbleSort(int* array, int length, bool verbose) {
+    int pass = 0;
+    int nSwaps = 0;
+    bool swapped = true;
+    // Continue while swaps were made in the last pass or until we have made length - 1 passes
+    while(swapped && pass < length - 1) {
+        swapped = false;
+        // Iterate until the length - pass th item of the array
+        for(int j = 0; j < length - pass - 1; j++) {
+            // Swap items if the first is smaller than the second
+            if(array[j] > array[j + 1]) {
+                swapped = true;
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                nSwaps++;
+            }
+        }
+        if(verbose) {
+            printf("\n\nPass %d: ", pass);
+            printArray(array, length);
+        }
+        pass++;
+    }
+    printf("\nBubble sort used: %d swaps", nSwaps);
+}
+
+void insertionSort(int* array, int length, bool verbose) {
+    int nSwaps = 0;
+    for(int i = 1; i < length; i++) { // Loops through array, i indicates the first unsorted item
+        int j = i;
+        int current = array[i]; // Store current item in a temporary variable
+        while(j > 0 && array[j - 1] > current) { // Start iterating from j downwards until array[j - 1] is less than the current item
+            if(verbose) printf("\nReplaced %d with %d: ", array[j], array[j - 1]);
+            array[j] = array[j - 1]; // Swap items whilst going down to create space
+            j--;
+            nSwaps++;
+            if(verbose) printArray(array, length);
+        }
+        if(verbose) printf("\nInserting %d, overwriting %d: ", current, array[j]);
+        array[j] = current; // Place the current item in the correct spot
+        if(verbose) printArray(array, length);
+    }
+    printf("\nInsertion sort used: %d swaps", nSwaps);
+}
+
+void mergeSort(const int* array, int length, int* output) {
+    if(length <= 1) {
+        // Base case
+        output[0] = array[0];
+        return;
+    };
+
+    // Determine size of each half
+    int leftSize = length / 2;
+    int rightSize = length - leftSize;
+
+    int left[leftSize];
+    int leftIndex = 0;
+    int right[rightSize];
+    int rightIndex = 0;
+
+    // Add elements to the correct half
+    for(int i = 0; i < length; i++) {
+        if(i < leftSize) {
+            left[leftIndex] = array[i];
+            leftIndex++;
+        } else {
+            right[rightIndex] = array[i];
+            rightIndex++;
+        }
+    }
+
+    // Recursively call the mergeSort method, splitting the list down
+    int newLeft[leftSize];
+    mergeSort(left, leftSize, newLeft);
+    int newRight[rightSize];
+    mergeSort(right, rightSize, newRight);
+
+    // Merge the results of the split arrays
+    mergeArrays(newLeft, leftSize, newRight, rightSize, output);
+}
+
+void mergeArrays(const int* left, int lenLeft, const int* right, int lenRight, int* output) {
+    int indexLeft = 0;
+    int indexRight = 0;
+    int outputIndex = 0;
+
+    // Whilst there is still elements in one of the arrays
+    while(indexLeft < lenLeft && indexRight < lenRight) {
+        // Compare the first remaining items
+        if(left[indexLeft] < right[indexRight]) {
+            output[outputIndex] = left[indexLeft];
+            indexLeft++;
+        } else {
+            output[outputIndex] = right[indexRight];
+            indexRight++;
+        }
+        outputIndex++;
+    }
+
+    // Add any remaining items from the arrays now that one is empty (one of these loops will not run at all)
+    for(int i = indexLeft; i < lenLeft; i++) {
+        output[outputIndex] = left[i];
+        outputIndex++;
+    }
+    for(int i = indexRight; i < lenRight; i++) {
+        output[outputIndex] = right[i];
+        outputIndex++;
+    }
+}
+
+void quickSort(int* array, int length) {
+    int high = array[length - 1];
+    int pivot = array[length - 1];
+}
